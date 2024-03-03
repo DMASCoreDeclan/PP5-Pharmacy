d97550763f89193ee4261c774dac6db91f434e01
DMASCoreDeclan Sweeney <declan.sweeney@gmail.com>
2024-03-03 10:18:12 +0000

    Add healthcare page

--- home/views.py
+++ home/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 def index(request):
     ''' A view to return the index page'''
     return render(request, 'home/index.html')
+
+
+def healthcare(request):
+    ''' A view to return the index page'''
+    return render(request, 'home/healthcare.html')
