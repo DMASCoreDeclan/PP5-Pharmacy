21cd15f306c99146fc4ea102f20e23014d2ca937
DMASCoreDeclan Sweeney <declan.sweeney@gmail.com>
2024-03-12 14:55:29 +0000

    Deploy to heroku

--- pharmacy/settings.py
+++ pharmacy/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 
@@ -29,6 +30,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-dmascoredec-pp5pharmacy-hlqf2lekwm8.ws-eu108.gitpod.io',
+    'phelans-pharmacy-bf69b3045245.herokuapp.com',
 ]
 
 # Application definition
@@ -104,13 +106,17 @@ WSGI_APPLICATION = 'pharmacy.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
