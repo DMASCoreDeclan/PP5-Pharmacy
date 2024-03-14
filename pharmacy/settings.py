6fb2d112f5f3a2a8ae0f8a9f701e9afb421536b3
DMASCoreDeclan Sweeney <declan.sweeney@gmail.com>
2024-03-14 10:14:39 +0000

    Fixed spelling mistake in settings.py

--- pharmacy/settings.py
+++ pharmacy/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'pharmacy.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-if 'DATABSE_URL' in os.environ:
+if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
@@ -108,6 +108,18 @@ else:
         }
     }
 
+# connect to SQLLITE
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# connect to CI DB
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
