7a12488bbcfa8b22bbd6cdf1b71c1a5302587170
DMASCoreDeclan Sweeney <declan.sweeney@gmail.com>
2024-03-26 11:53:41 +0000

    Added stripe, js, settings and views for checkout functionality

--- pharmacy/settings.py
+++ pharmacy/settings.py
@@ -174,15 +174,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
 MEDIA_URL = '/media/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Stripe 
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+
+STRIPE_CURRENCY ='eur'
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 
 if 'USE_AWS' in os.environ:
     # Cache control
