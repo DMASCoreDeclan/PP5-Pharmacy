b4bc66afef77a9f919561066a7fe8dafcb58df45
DMASCoreDeclan Sweeney <declan.sweeney@gmail.com>
2024-03-31 11:11:02 +0000

    Added profile app, views, models, urls, css and html

--- pharmacy/urls.py
+++ pharmacy/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('cartview/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
