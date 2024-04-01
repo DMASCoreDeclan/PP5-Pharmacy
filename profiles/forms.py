f44a6bcaee88318cc936c66215005912fc9eaa38
DMASCoreDeclan Sweeney <declan.sweeney@gmail.com>
2024-04-01 16:41:38 +0000

    Added order history amd update checkout views

--- profiles/forms.py
+++ profiles/forms.py
@@ -12,7 +12,7 @@ class UserProfileForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         """
         Add placeholders and classes, remove auto-generted
-        labels and set autofocus on first fiels
+        labels and set autofocus on first fields
         """
         super().__init__(*args, **kwargs)
         placeholders = {
--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from .models import UserProfile
+from checkout.models import Order
 from .forms import UserProfileForm
-
 
 def profiles(request):
     """
@@ -24,6 +24,22 @@ def profiles(request):
         'form': form,
         'orders': orders,
         'on_profile_page': True,
+    }
+
+    return render(request, template, context)
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for {order_number}.  '
+        'A confirmation email was sent on that date'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
