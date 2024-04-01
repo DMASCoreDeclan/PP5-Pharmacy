4348471b575cdeabca4fb4ee7d5cc63934091aa0
DMASCoreDeclan Sweeney <declan.sweeney@gmail.com>
2024-04-01 17:26:05 +0000

    Added email confirmation to webhook_handler

--- pharmacy/settings.py
+++ pharmacy/settings.py
@@ -187,6 +187,7 @@ STRIPE_CURRENCY ='eur'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
+DEFAULT_FROM_EMAIL = 'phelanspharmacy@example.com'
 
 if 'USE_AWS' in os.environ:
     # Cache control
