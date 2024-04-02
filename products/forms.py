a4bcf9826a5ff952c8a35c04d9c6a6e910bcce07
DMASCoreDeclan Sweeney <declan.sweeney@gmail.com>
2024-04-02 11:20:09 +0000

    Added add_product urls, views and html

--- products/forms.py
+++ products/forms.py
@@ -1,11 +1,11 @@
 from django import forms
-from .models import Produt, Category
+from .models import Product, Category
 
 
 class ProductForm(forms.ModelForm):
 
     class Meta:
-        model = ProductForm
+        model = Product
         fields = '__all__'
 
     def __init__(self, *args, **kwargs):
