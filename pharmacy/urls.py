c7ac323cbd66bd8f89f9cf99018b4ac5dabc384f
DMASCoreDeclan Sweeney <declan.sweeney@gmail.com>
2024-04-25 08:50:09 +0000

    Update nav links, urls and html for order_px,html

--- pharmacy/urls.py
+++ pharmacy/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('cartview/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path('prescription/', include('prescription.urls')),
     path('summernote/', include('django_summernote.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- prescription/views.py
+++ prescription/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
+
+
+def order_px(request):
+
+    template = "prescription/order_px.html"
+    # context = {
+    #     'form': form
+    # }
+
+    return render(request, template)
