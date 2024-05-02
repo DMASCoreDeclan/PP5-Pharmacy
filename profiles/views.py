7a9ddf8947a594bb3d37480b6ef6e38b1ce77430
DMASCoreDeclan Sweeney <declan.sweeney@gmail.com>
2024-05-02 18:37:55 +0000

    Add px_order history detail

--- profiles/views.py
+++ profiles/views.py
@@ -71,3 +71,42 @@ def px_order_history(request, px_order_number):
     }
 
     return render(request, template, context)
+
+
+def prescription_history(request, px_order_number):
+    """
+    displays the PX submitted
+    """
+    px_order = get_object_or_404(Prescription, px_order_number=px_order_number)
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attache the users order to the profile
+        prescription.user = profile
+        # prescription.save()
+
+        # Save the users info
+        # if save_info:
+        #     profile_data = {
+        #     'default_phone_number': order.phone_number, 
+        #     'default_street_address1': order.street_address1, 
+        #     'default_street_address2': order.street_address2, 
+        #     'default_town_or_city': order.town_or_city, 
+        #     'default_postcode': order.postcode, 
+        #     'default_county':  order.county, 
+        #     'default_country': order.country, 
+        # }
+        user_profile_form = UserProfileForm(profile_data, instance=profile)
+        if user_profile_form.is_valid():
+            user_profile_form.save()
+
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}.  A confirmation \
+        email will be sent to {order.email}.')
+
+    template = 'prescription/prescription_history.html'
+    context = {
+        'px_order': px_order,
+    }
+
+    return render(request, template, context)
