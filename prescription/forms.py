de4fa3d71020634d91b83439878ef1c08d5e60da
DMASCoreDeclan Sweeney <declan.sweeney@gmail.com>
2024-05-07 13:33:39 +0000

    Add PX Admin functionality to forms, views, urls, px_admin/mgmt-nav.html

--- prescription/forms.py
+++ prescription/forms.py
@@ -12,14 +12,21 @@ PX_DELIVERY = [
         ('1', 'Delivery'),
     ]
 
-class PrescriptionForm(forms.ModelForm):
-
+class PxForm(forms.ModelForm):
+    '''
+    Form for px_order.html - to allow Users to input their details and upload 
+    an image file representing their PX from their
+    ''' 
     class Meta:
         model = Prescription
         fields = '__all__'
-        exclude = ['user', 'px_status', 'order_form', ]
+        exclude = ['user', 'px_status', ]
 
-    px_image = forms.ImageField(label='PrescriptionImage', required=True, widget=CustomClearableFileInput)
+    px_image = forms.ImageField(
+        label='PrescriptionImage', 
+        required=True, 
+        widget=CustomClearableFileInput
+        )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -27,3 +34,41 @@ class PrescriptionForm(forms.ModelForm):
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-purple rounded-0'
 
+
+class PxAdminForm(forms.ModelForm):
+    '''
+    Form for px_admin.html - to allow the Pharmacy Team to review PXs to be 
+    prepared
+    ''' 
+
+    class Meta:
+        model = Prescription
+        fields = '__all__'
+        exclude = ['user', 'px_status', 'order_form', ]
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            
+            for field_name, field in self.fields.items():
+                field.widget.attrs['class'] = 'border-purple rounded-0'
+
+
+class PxChangeStatusForm(forms.ModelForm):
+    '''
+    Form for edit_px_status.html - to allow The Paharmacy Team to view the PX 
+    and update the PX Order Status
+    ''' 
+    class Meta:
+        model = Prescription
+        fields = '__all__'
+        exclude = [
+            'user',
+            'px_delivery',
+            'px_image', 
+            ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+            
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-purple rounded-0'
--- prescription/views.py
+++ prescription/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-from .forms import PrescriptionForm
+from .forms import PxForm, PxAdminForm, PxChangeStatusForm
 from profiles.models import UserProfile
 from .models import Prescription 
 # Create your views here.
@@ -20,14 +20,17 @@ def order_px(request):
     if not request.user.is_authenticated:
         messages.error(request, 'You must be logged in to use this service, \
             please login')
-        return redirect(reverse('login'))
+        return redirect(reverse('account_login'))
+
+    if request.user.is_staff:
+        messages.error(request, 'Team members should not order PXs this way!')
+        return redirect(reverse('px_admin'))
 
     if request.method == 'POST':
-        form = PrescriptionForm(request.POST, request.FILES, request.user)
+        form = PxForm(request.POST, request.FILES, request.user)
         if form.is_valid():
             form.save(commit=False)
             form.instance.user = request.user
-            form.user_profile = request.user
             form.save()
             messages.success(
                 request, 
@@ -41,7 +44,7 @@ def order_px(request):
                     Your Prescription has not been sent!\
                         Please try again.')
     else:
-        form = PrescriptionForm()
+        form = PxForm()
 
     template = "prescription/px_order.html"
     context = {
@@ -51,26 +54,20 @@ def order_px(request):
     return render(request, template, context)
 
 
+@login_required
 def px_admin(request):
     '''
-    Display PX Orders for processing turn To Be Processed -> Processed
+    Display PX Orders for processing turn To Be Processed -> Processed by a
+    member of the Team.
     '''
+    if not request.user.is_staff:
+        messages.error(request, 'You must be logged in as a member of our team\
+            to use this service.')
+        return redirect(reverse('account_login'))
+
     PX_STATUS = dict(Prescription.PX_STATUS)
     form = Prescription.objects.all()
-  
-    if request.method == 'GET':
-        Prescription.objects.all()
-    
-    if request.method == 'POST':
-        form = Prescription(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(
-                request, 
-                f'Your Prescription has been successfully sent!'
-                )
-            return redirect(reverse('px_admin'))
-  
+
     context = {
         'form': form, 
         'PX_STATUS': PX_STATUS, 
@@ -78,4 +75,37 @@ def px_admin(request):
 
     return render(request, 'prescription/px_admin.html', context)
 
+@login_required
+def edit_px_status(request, px_id):
+    '''
+    View to edit the status of the PX from Neing Pocessed to Processed'''
+    if not request.user.is_staff:
+        messages.error(request, 'You must be logged in as a member of our team\
+            to use this service.')
+        return redirect(reverse('account_login'))
 
+    px = get_object_or_404(Prescription, pk=px_id)
+    # strips the px_image formatting created in Form PxForm
+    px_status_image = px.px_image
+    if request.method == 'POST':
+        px_form = PxChangeStatusForm(request.POST, request.FILES, instance=px)
+        if px_form.is_valid():
+            px_form.save()
+            messages.success(request, 'Successfully updated PX status!')
+            return redirect(reverse('px_admin'))
+        else:
+            messages.error(request, 'Failed to update the PX status. Please \
+                ensure the form is valid.')
+    else:
+        px_form = PxChangeStatusForm(instance=px)
+        # messages.info(request, f'You are editing {px_order_number}')
+
+    template = 'prescription/edit_px_status.html'
+    context = {
+        'px_form': px_form,
+        'px': px,
+        'px_status_image': px_status_image,
+    }
+
+    return render(request, template, context)
+    
