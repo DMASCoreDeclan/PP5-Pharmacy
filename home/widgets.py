181b7ba8cb2c957b734746f152bd67d4fa188580
DMASCoreDeclan Sweeney <declan.sweeney@gmail.com>
2024-05-12 08:25:27 +0000

    Fix widgets after pylinter changes

--- home/widgets.py
+++ home/widgets.py
@@ -6,13 +6,14 @@ class CustomClearableFileInput(ClearableFileInput):
     clear_checkbox_label = _('Remove')
     initial_text = _('Current Image')
     input_text = _('Select Image')
-    template_name = 'home/custom_widget_templa\
-        te/custom_clearable_file_input.html'
+    name = 'home/custom_widget_template/custom_clearable_file_input.html'
+    template_name = name
 
 
 class CustomClearableIconInput(ClearableFileInput):
     clear_checkbox_label = _('Remove')
     initial_text = _('Current Image')
     input_text = _('Select Icon')
-    template_name = 'home/custom_widget_templa\
-        te/custom_clearable_file_input.html'
+    name = 'home/custom_widget_template/custom_clearable_file_input.html'
+    template_name = name
+    
