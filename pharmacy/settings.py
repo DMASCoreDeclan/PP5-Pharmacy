d493dd5b10f4615dedc9b14167370d4961f3dabb
DMASCoreDeclan Sweeney <declan.sweeney@gmail.com>
2024-05-15 19:42:54 +0000

    Fix settings.py print secrets

--- pharmacy/settings.py
+++ pharmacy/settings.py
@@ -187,9 +187,6 @@ STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET')
 DEFAULT_FROM_EMAIL = 'phelanspharmacy@example.com'
-print(STRIPE_WH_SECRET)
-print(STRIPE_SECRET_KEY)
-print(STRIPE_PUBLIC_KEY)
 
 if 'USE_AWS' in os.environ:
     # Cache control
